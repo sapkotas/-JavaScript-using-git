8fc1815097bb51f1e8a32019ff9b0afd8800c40c
Sunav Sapkota <sunavsapkota57@gmail.com>
2024-01-29 19:06:47 +0545

    Divisible

--- JavaScript/2_numberDivisible.js
+++ JavaScript/2_numberDivisible.js
@@ -5,7 +5,7 @@ const prompt = require('prompt-sync')();
   let a = prompt("Enter a number: ");
 a = Number.parseInt(a);
 if (a % 2 == 0 || a % 3 == 0) {
-  console.log("The number is divisible by 2 and 3");
+  console.log("The number is divisible by 2 or 3");
 } else {
-  console.log("The number is not divisible by 2 and 3");
+  console.log("The number is not divisible by 2 or 3");
 }

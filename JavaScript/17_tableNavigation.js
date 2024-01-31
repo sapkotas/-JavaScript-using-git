091afbbc616eaac5b416a2c6a87c5612bf3a91ec
Sunav Sapkota <sunavsapkota57@gmail.com>
2024-01-31 14:38:03 +0545

    JS

---
+++ JavaScript/17_tableNavigation.js
@@ -0,0 +1,12 @@
+let t = document.body.firstElementChild.firstElementChild;
+console.log(t)//prints the table
+console.log(t.rows)//prints the rows of the table
+console.log(t.caption)//prints the caption of the table
+console.log(t.tHead.firstElementChild)//prints the first element of the table head
+console.log(t.tBodies)//prints the table body
+
+console.log(t.rows[0].rowIndex)//prints the index of the 
+//console.log(t.)
+
+console.log(typeof document);
+console.log(typeof window)
